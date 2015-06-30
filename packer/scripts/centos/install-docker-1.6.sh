b5102b9badd7838371393896bf6568608eb65f12
Jimmi Dyson <jimmidyson@gmail.com>
2015-06-30 08:56:31 +0100

    Update devicemapper stuff

--- packer/scripts/centos/install-docker-1.6.sh
+++ packer/scripts/centos/install-docker-1.6.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-yum install -y http://cbs.centos.org/kojifiles/packages/docker/1.6.2/4.gitc3ca5bb.el7/x86_64/docker-1.6.2-4.gitc3ca5bb.el7.x86_64.rpm
+yum install -y cloud-utils-growpart http://cbs.centos.org/kojifiles/packages/docker/1.6.2/4.gitc3ca5bb.el7/x86_64/docker-1.6.2-4.gitc3ca5bb.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/docker-storage-setup/0.5/3.el7.centos/noarch/docker-storage-setup-0.5-3.el7.centos.noarch.rpm
 systemctl enable docker
 groupadd docker
 usermod -a -G docker vagrant
+systemctl start docker-storage-setup
 
 cat >> /etc/security/limits.conf <<EOF
 *        hard    nproc           8192
@@ -13,5 +14,3 @@ cat >> /etc/security/limits.conf <<EOF
 EOF
 
 sed -i "s|^OPTIONS=.*|OPTIONS='--selinux-enabled -H unix://var/run/docker.sock -H tcp://0.0.0.0:2375 --insecure-registry=172.0.0.0/8 --log-level=warn'|" /etc/sysconfig/docker
-
-sed -i "s|^DOCKER_STORAGE_OPTIONS=.*$|DOCKER_STORAGE_OPTIONS='-s devicemapper --storage-opt dm.fs=xfs --storage-opt dm.thinpooldev=/dev/mapper/root-docker--pool'|" /etc/sysconfig/docker-storage
