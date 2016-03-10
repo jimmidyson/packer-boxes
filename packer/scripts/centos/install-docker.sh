03e7522653ba9818b391cdaa72c5cb0d10d3ee1b
Jimmi Dyson <jimmidyson@gmail.com>
2016-03-10 10:24:41 +0000

    Force Docker version to 1.9.1

--- packer/scripts/centos/install-docker.sh
+++ packer/scripts/centos/install-docker.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-curl -sSL https://get.docker.com/ | sh
+cat > /etc/yum.repos.d/docker-main.repo <<EOF
+[docker-main-repo]
+name=Docker Main Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum install -y docker-engine-1.9.1 docker-engine-selinux-1.9.1
 
 mkdir /etc/systemd/system/docker.service.d/
 cat >> /etc/systemd/system/docker.service.d/override.conf <<EOF
